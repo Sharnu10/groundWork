d7effe8613de1bea079344bf99509fa9752f42f0
Sharnu Ravindra Narsagol <sharanuvishaln@gmail.com>
2023-11-10 11:43:07 +0530

    Created UI screen to enter city name
    - Installed bootstrap.
    - Imported bootstrap css.
    - Created screen to show weather info and send city name.

--- apps/weather/src/app/app.component.ts
+++ apps/weather/src/app/app.component.ts
@@ -9,23 +9,25 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'weather';
   weatherData: any;
+  cityName = 'Seram';
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.getWeatherData('Seram');
+    this.getWeatherData();
   }
 
   //  function to  get weather data for requested city.
-  getWeatherData(city: string): void {
-    this.http.get(`http://localhost:3030/api/weather/${city}`).subscribe({
-      next: (data: any) => {
-        console.log('data ', data);
-        this.weatherData = data.weatherData;
-      },
-      error: (error) => {
-        console.log('error ', error);
-      },
-    });
+  getWeatherData(): void {
+    this.http
+      .get(`http://localhost:3030/api/weather/${this.cityName}`)
+      .subscribe({
+        next: (data: any) => {
+          this.weatherData = data.weatherData;
+        },
+        error: (error) => {
+          console.log('error ', error);
+        },
+      });
   }
 }
