77b93ad5982fc43185d3cd12df6d513b495a0d28
Sharnu Ravindra Narsagol <sharanuvishaln@gmail.com>
2023-11-10 11:43:07 +0530

    Created UI screen to enter city name
    - Installed bootstrap.
    - Imported bootstrap css.
    - Created screen to show weather info and send city name.

--- apps/weather/src/app/app.module.ts
+++ apps/weather/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
@@ -12,6 +13,7 @@ import { appRoutes } from './app.routes';
   imports: [
     BrowserModule,
     HttpClientModule,
+    FormsModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
   ],
   providers: [],
